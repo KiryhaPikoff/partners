3eb29bf8896310d18285a8beda97f430bede7810
KiryhaPikoff <pikov_kirya@mail.ru>
2020-08-26 14:09:19 +0300

    Initial commit.

---
+++ src/main/java/com/mediasoft/partnersservice/PartnersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mediasoft.partnersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnersServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediasoft/partnersservice/PartnersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediasoft.partnersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
